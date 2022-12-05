387fdff02399481982c225cc2ce780fe56f8b6ca
Ran <ranking.han@jiahui.com>
2022-12-05 17:58:06 +0800

    feat: init project

---
+++ src/test/java/com/hank/docker/AlgorithmPracticeTests.java
@@ -0,0 +1,13 @@
+package com.hank.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmPracticeTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
